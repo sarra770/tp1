3469ceacdc42bfdadeeacb454da308fdd0f8f155
sarra770 <73799109+sarra770@users.noreply.github.com>
2021-11-29 11:40:02 +0100

    Add files via upload

---
+++ produits/src/main/java/com/example/produits/service/ProduitService.java
@@ -0,0 +1,14 @@
+package com.example.produits.service;
+import java.util.List;
+import com.example.produits.entities.Produit;
+
+public interface ProduitService {
+	Produit saveProduit(Produit p);
+	Produit updateProduit(Produit p);
+	void deleteProduit(Produit p);
+	 void deleteProduitById(Long id);
+	Produit getProduit(Long id);
+	List<Produit> getAllProduits();
+	}
+
+
